690e80028eb682001b78c9b9a7862b4f93a80972
sonai99 <anirbandutta428@gmail.com>
2023-07-01 19:04:13 +0530

    todays commit

---
+++ new.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 10
+	y := 20
+	// c := x+y;
+	// fmt.Printf("Hello world \n")
+	fmt.Println(x + y + 10)
+	name := "Anirban"
+	surname := "Dutta"
+	fmt.Println(name + " " + surname + "Is a good coder")
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("*")
+	}
+
+}
